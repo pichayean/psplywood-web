432b2b990c1bd8a900fb7fac7501fcf2b25b64c5
pichayean <pichayeanyensiri@gmail.com>
2024-02-11 21:30:26 +0700

    first commit

---
+++ PSPlywoodWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSPlywoodWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
